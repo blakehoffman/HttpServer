321f721a3df7b550fc3b21cb57add0fcd8ea0bc7
blakehoffman <hoffman9134>
2023-05-24 06:56:25 -0500

    finished unit tests for status line parsing

--- http/http_parser.go
+++ http/http_parser.go
@@ -20,45 +20,44 @@ type requestLine struct {
 }
 
 type httpParseResult[T any] struct {
-	result T
-	lastByte byte
+	result    T
+	lastByte  byte
 	completed bool
 }
 
-func get_status_line_parse_location(lastByte byte, requestLine requestLine) int{
-	if lastByte == 0{
-		return InVerb;
+func get_status_line_parse_location(lastByte byte, requestLine requestLine) int {
+	if lastByte == 0 {
+		return InVerb
 	}
 
-	if lastByte == ' '{
-		if requestLine.verb != "" && requestLine.url ==  ""{
+	if lastByte == ' ' {
+		if requestLine.verb != "" && requestLine.url == "" {
 			return InUrl
-		} else{
+		} else {
 			return InVersion
 		}
 	}
 
-	if requestLine.version != ""{
+	if requestLine.version != "" {
 		return InVersion
-	} else if requestLine.url != ""{
+	} else if requestLine.url != "" {
 		return InUrl
-	} else{
+	} else {
 		return InVerb
 	}
 }
 
 func parse_http_status_line(buffer []byte, parseResult httpParseResult[requestLine]) (httpParseResult[requestLine], *byte) {
-	var httpRequestLine requestLine
 	var dataStringBuilder strings.Builder
 	var lastByte byte
 	parseLocation := get_status_line_parse_location(parseResult.lastByte, parseResult.result)
 
 	// if part of the string was written from the last call to this method, make sure to concatenate it
-	if parseLocation == InVerb && parseResult.result.verb != ""{
+	if parseLocation == InVerb && parseResult.result.verb != "" {
 		dataStringBuilder.WriteString(parseResult.result.verb)
-	} else if parseLocation == InUrl && parseResult.result.url != ""{
+	} else if parseLocation == InUrl && parseResult.result.url != "" {
 		dataStringBuilder.WriteString(parseResult.result.url)
-	} else if parseLocation == InVersion && parseResult.result.version != ""{
+	} else if parseLocation == InVersion && parseResult.result.version != "" {
 		dataStringBuilder.WriteString(parseResult.result.version)
 	}
 
@@ -68,47 +67,47 @@ func parse_http_status_line(buffer []byte, parseResult httpParseResult[requestLi
 		   know we've hit the end of the message within the buffer since we are hitting
 		   the default values
 		*/
-		if currentByte == 0{
+		if currentByte == 0 {
 			return httpParseResult[requestLine]{
-				result: httpRequestLine,
+				result:   parseResult.result,
 				lastByte: 0,
 			}, nil
 		}
-		
-		if currentByte == ' '{
-			write_string_builder_to_request_line(parseLocation, dataStringBuilder, &httpRequestLine)
+
+		if currentByte == ' ' {
+			write_string_builder_to_request_line(parseLocation, dataStringBuilder, &parseResult.result)
 			parseLocation++
 			dataStringBuilder.Reset()
-		} else if currentByte == '\n' && lastByte == '\r'{
+		} else if currentByte == '\n' && lastByte == '\r' {
 			lastByte = currentByte
-			write_string_builder_to_request_line(parseLocation, dataStringBuilder, &httpRequestLine)
+			write_string_builder_to_request_line(parseLocation, dataStringBuilder, &parseResult.result)
 
 			return httpParseResult[requestLine]{
-				result: httpRequestLine,
-				lastByte: lastByte,
+				result:    parseResult.result,
+				lastByte:  lastByte,
 				completed: true,
 			}, &buffer[i]
-		} else if currentByte != '\n' && currentByte != '\r'{
+		} else if currentByte != '\n' && currentByte != '\r' {
 			dataStringBuilder.WriteByte(currentByte)
 		}
 
 		lastByte = currentByte
 	}
 
-	write_string_builder_to_request_line(parseLocation, dataStringBuilder, &httpRequestLine)
-	
+	write_string_builder_to_request_line(parseLocation, dataStringBuilder, &parseResult.result)
+
 	return httpParseResult[requestLine]{
-		result: httpRequestLine,
+		result:   parseResult.result,
 		lastByte: lastByte,
 	}, nil
 }
 
 func write_string_builder_to_request_line(parseLocation int, stringBuilder strings.Builder, requestLinePointer *requestLine) {
-	if parseLocation == InVerb{
+	if parseLocation == InVerb {
 		requestLinePointer.verb = stringBuilder.String()
-	} else if parseLocation == InUrl{
+	} else if parseLocation == InUrl {
 		requestLinePointer.url = stringBuilder.String()
-	} else if parseLocation == InVersion{
+	} else if parseLocation == InVersion {
 		requestLinePointer.version = stringBuilder.String()
 	}
 }
--- http/http_parser_test.go
+++ http/http_parser_test.go
@@ -4,24 +4,25 @@ import (
 	"testing"
 )
 
-func Test_Get_Status_Line_Parse_Location_With_Character_Last_Byte_And_Non_Empty_Version_Returns_InUrl(t *testing.T) {
+func Test_Get_Status_Line_Parse_Location_With_Character_Last_Byte_And_Non_Empty_Url_Returns_InUrl(t *testing.T) {
 	requestLine := requestLine{
-		url: "test",
+		verb: "GET",
+		url:  "test",
 	}
 
-	actual := get_status_line_parse_location('a', requestLine)
+	actual := get_status_line_parse_location('t', requestLine)
 
 	if actual != InUrl {
 		t.Fatalf("get_status_line_parse_location('a', requestLine) = %d, want %d", actual, InUrl)
 	}
 }
 
-func Test_Get_Status_Line_Parse_Location_With_Character_Last_Byte_And_Non_Empty_Version_Returns_InVerb(t *testing.T) {
+func Test_Get_Status_Line_Parse_Location_With_Character_Last_Byte_And_Non_Empty_Verb_Returns_InVerb(t *testing.T) {
 	requestLine := requestLine{
 		verb: "test",
 	}
 
-	actual := get_status_line_parse_location('a', requestLine)
+	actual := get_status_line_parse_location('t', requestLine)
 
 	if actual != InVerb {
 		t.Fatalf("get_status_line_parse_location('a', requestLine) = %d, want %d", actual, InVerb)
@@ -30,10 +31,12 @@ func Test_Get_Status_Line_Parse_Location_With_Character_Last_Byte_And_Non_Empty_
 
 func Test_Get_Status_Line_Parse_Location_With_Character_Last_Byte_And_Non_Empty_Version_Returns_InVersion(t *testing.T) {
 	requestLine := requestLine{
+		verb:    "GET",
+		url:     "test.html",
 		version: "test",
 	}
 
-	actual := get_status_line_parse_location('a', requestLine)
+	actual := get_status_line_parse_location('t', requestLine)
 
 	if actual != InVersion {
 		t.Fatalf("get_status_line_parse_location('a', requestLine) = %d, want %d", actual, InVersion)
@@ -51,8 +54,9 @@ func Test_Get_Status_Line_Parse_Location_With_Zero_Last_Byte_Returns_InVerb(t *t
 }
 
 func Test_Get_Status_Line_Parse_Location_With_Space_Last_Byte_Returns_InUrl(t *testing.T) {
-	requestLine := requestLine{}
-	requestLine.verb = "get"
+	requestLine := requestLine{
+		verb: "GET",
+	}
 
 	actual := get_status_line_parse_location(' ', requestLine)
 
@@ -87,5 +91,96 @@ func Test_Parse_Http_Status_Line_With_Valid_Line_Returns_Status_Line(t *testing.
 
 	if actual.result != expected {
 		t.Fatalf("parse_http_status_line(), got %v want %v", actual.result, expected)
+	}
+}
+
+func Test_Parse_Http_Status_Line_With_Space_As_Last_Byte_Between_Returns_Status_Line(t *testing.T) {
+	buffer := []byte{'t', 'e', 's', 't', '.', 'h', 't', 'm', 'l', ' ', 'H', 'T', 'T', 'P', '/', '1', '.', '1', '\r', '\n'}
+	parseResult := httpParseResult[requestLine]{
+		lastByte: ' ',
+		result: requestLine{
+			verb: "GET",
+		},
+	}
+
+	expected := requestLine{
+		verb:    "GET",
+		url:     "test.html",
+		version: "HTTP/1.1",
+	}
+
+	actual, _ := parse_http_status_line(buffer, parseResult)
+
+	if actual.result != expected {
+		t.Fatalf("parse_http_status_line() with space as last byte, got %v want %v", actual.result, expected)
+	}
+}
+
+func Test_Parse_Http_Status_Line_With_Partial_Url_Returns_Status_Line(t *testing.T) {
+	buffer := []byte{'s', 't', '.', 'h', 't', 'm', 'l', ' ', 'H', 'T', 'T', 'P', '/', '1', '.', '1', '\r', '\n'}
+	parseResult := httpParseResult[requestLine]{
+		lastByte: 'e',
+		result: requestLine{
+			verb: "GET",
+			url:  "te",
+		},
+	}
+
+	expected := requestLine{
+		verb:    "GET",
+		url:     "test.html",
+		version: "HTTP/1.1",
+	}
+
+	actual, _ := parse_http_status_line(buffer, parseResult)
+
+	if actual.result != expected {
+		t.Fatalf("parse_http_status_line() with partial url, got %v want %v", actual.result, expected)
+	}
+}
+
+func Test_Parse_Http_Status_Line_With_Partial_Verb_Returns_Status_Line(t *testing.T) {
+	buffer := []byte{'T', ' ', 't', 'e', 's', 't', '.', 'h', 't', 'm', 'l', ' ', 'H', 'T', 'T', 'P', '/', '1', '.', '1', '\r', '\n'}
+	parseResult := httpParseResult[requestLine]{
+		lastByte: 'E',
+		result: requestLine{
+			verb: "GE",
+		},
+	}
+
+	expected := requestLine{
+		verb:    "GET",
+		url:     "test.html",
+		version: "HTTP/1.1",
+	}
+
+	actual, _ := parse_http_status_line(buffer, parseResult)
+
+	if actual.result != expected {
+		t.Fatalf("parse_http_status_line() with partial verb, got %v want %v", actual.result, expected)
+	}
+}
+
+func Test_Parse_Http_Status_Line_With_Partial_Version_Returns_Status_Line(t *testing.T) {
+	buffer := []byte{'T', 'P', '/', '1', '.', '1', '\r', '\n'}
+	parseResult := httpParseResult[requestLine]{
+		lastByte: 'T',
+		result: requestLine{
+			verb:    "GET",
+			url:     "test.html",
+			version: "HT",
+		},
+	}
+
+	expected := requestLine{
+		verb:    "GET",
+		url:     "test.html",
+		version: "HTTP/1.1",
+	}
+
+	actual, _ := parse_http_status_line(buffer, parseResult)
+
+	if actual.result != expected {
+		t.Fatalf("parse_http_status_line() with partial version, got %v want %v", actual.result, expected)
 	}
 }
