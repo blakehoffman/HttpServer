a4a6aa9b1525d5833d1c0ef9a6d4471e56d690bf
blakehoffman <hoffman9134>
2023-06-02 06:59:05 -0500

    more work on parsing http headers

--- http/http_parser.go
+++ http/http_parser.go
@@ -27,6 +27,8 @@ type requestLine struct {
 type httpParseHeadersResult struct {
 	httpParseResult[[]httpHeader]
 	atNewHeader bool
+	atHeaderValue bool
+	headerParseIndex int
 }
 
 type httpParseResult[T any] struct {
@@ -58,12 +60,37 @@ func get_status_line_parse_location(lastByte byte, requestLine requestLine) int 
 }
 
 func parse_http_headers(buffer []byte, parseResult httpParseHeadersResult) (httpParseHeadersResult, *byte) {
+	var dataStringBuilder strings.Builder
+	lastByte := parseResult.lastByte
+	write_http_partial_header_to_string_builder(parseResult, dataStringBuilder)
+
+	for i := 0; i <= len(buffer); i++ {
+		currentByte := buffer[i]
+
+		// if current byte is zero, we know we've hit the end of data in the buffer
+		if currentByte == 0{
+			return httpParseHeadersResult {
+				httpParseResult: parseResult.httpParseResult,
+				atNewHeader: parseResult.atNewHeader,
+				atHeaderValue: parseResult.atHeaderValue,
+				headerParseIndex: parseResult.headerParseIndex,
+			}, nil
+		} else if parseResult.atNewHeader && parseResult.lastByte == '\r' && currentByte == '\n'{
+			write_string_builder_to_http_header(&parseResult.result[parseResult.headerParseIndex], parseResult.atHeaderValue, dataStringBuilder)
+			lastByte = currentByte
+
+			return httpParseHeadersResult { 
+				httpParseResult: parseResult.httpParseResult,
+			}, &buffer[i]
+		}
+	}
+
 	return httpParseHeadersResult{}, nil
 } 
 
 func parse_http_status_line(buffer []byte, parseResult httpParseResult[requestLine]) (httpParseResult[requestLine], *byte) {
 	var dataStringBuilder strings.Builder
-	var lastByte byte
+	lastByte := parseResult.lastByte
 	parseLocation := get_status_line_parse_location(parseResult.lastByte, parseResult.result)
 
 	// if part of the string was written from the last call to this method, make sure to concatenate it
@@ -82,13 +109,8 @@ func parse_http_status_line(buffer []byte, parseResult httpParseResult[requestLi
 		   the default values
 		*/
 		if currentByte == 0 {
-			return httpParseResult[requestLine]{
-				result:   parseResult.result,
-				lastByte: 0,
-			}, nil
-		}
-
-		if currentByte == ' ' {
+			return parseResult, nil
+		} else if currentByte == ' ' {
 			write_string_builder_to_request_line(parseLocation, dataStringBuilder, &parseResult.result)
 			parseLocation++
 			dataStringBuilder.Reset()
@@ -114,6 +136,22 @@ func parse_http_status_line(buffer []byte, parseResult httpParseResult[requestLi
 		result:   parseResult.result,
 		lastByte: lastByte,
 	}, nil
+}
+
+func write_http_partial_header_to_string_builder(parseResult httpParseHeadersResult, dataStringBuilder strings.Builder){
+	if !parseResult.atNewHeader && !parseResult.atHeaderValue {
+		dataStringBuilder.WriteString(parseResult.httpParseResult.result[parseResult.headerParseIndex].name)
+	} else if !parseResult.atNewHeader && parseResult.atHeaderValue {
+		dataStringBuilder.WriteString(parseResult.httpParseResult.result[parseResult.headerParseIndex].value)
+	}
+}
+
+func write_string_builder_to_http_header(httpHeader *httpHeader, atHeaderValue bool, stringBuilder strings.Builder) {
+	if atHeaderValue {
+		httpHeader.value = stringBuilder.String()
+	} else {
+		httpHeader.name = stringBuilder.String()
+	}
 }
 
 func write_string_builder_to_request_line(parseLocation int, stringBuilder strings.Builder, requestLinePointer *requestLine) {
